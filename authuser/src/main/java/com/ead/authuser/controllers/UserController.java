dd48c236709a3d95832d709df3ea301f7915cb2a
Samille Machado <sami.bmachado@gmail.com>
2025-05-27 22:16:53 -0300

    create api methods

--- authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -1,8 +1,13 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
@@ -12,5 +17,21 @@ public class UserController {
 
     public UserController(final UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserModel>> getAllUsers() {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId") UUID userId) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findById(userId).get());
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Object> deleteUer(@PathVariable(value = "userId") UUID userId) {
+        userService.delete(userService.findById(userId).get());
+        return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully.");
     }
 }
--- authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,8 +1,14 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -11,6 +17,21 @@ public class UserServiceImpl implements UserService {
 
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return userRepository.findById(userId);
+    }
+
+    @Override
+    public void delete(UserModel userModel) {
+        userRepository.delete(userModel);
     }
 
 }
